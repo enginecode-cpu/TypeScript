a4408e0f28a4b5dd7115e292848fdda48385ce62
enginecode-cpu <bassmatics1002@gmail.com>
2020-10-21 23:31:54 +0900

    클래스와 인터페이스

--- src/3.객체와 클래스.ts
+++ src/3.객체와 클래스.ts
@@ -42,3 +42,40 @@ console.log(hyungman2);
  *      }
  * }
  */
+
+/**
+ * 인터페이스 구현
+ * 클래스가 인터페이스를 구현할 때는 implements 키워드를 사용한다
+ * 
+ * class 클래스 이름 implements 인터페이스 이름 {
+ *     ...
+ * }
+ * 
+ * 클래스 몸통에는 반드시 인터페이스가 정의하고 있는 속성을 
+ * 멤버 속성으로 포함해야한다.
+ */
+
+interface IPerson3 {
+    name: string
+    age?: number
+}
+
+class Person3 implements IPerson3 {
+    name: string
+    age: number
+}
+
+/**
+ * 다음과 같이 해도 된다.
+ */
+interface IPerson4 {
+    name: string
+    age?: number
+}
+
+class Person4 implements IPerson4 {
+    constructor(public name: string, public age: number) {}
+}
+
+let jjang_gu: Person4 = new Person4('신짱구', 5);
+console.log(jjang_gu);
