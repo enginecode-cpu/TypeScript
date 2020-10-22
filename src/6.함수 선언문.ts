2efc1fbfe05f8a4b18cf0816d0efa737d4e8465e
enginecode-cpu <bassmatics1002@gmail.com>
2020-10-22 23:17:35 +0900

    type 키워드로 별칭 만들기

--- src/6.함수 선언문.ts
+++ src/6.함수 선언문.ts
@@ -28,3 +28,16 @@ function print(name: string, age: number): void {
 let print2: (name: string, age: number) => void = function (name: string, age: number): void {
     console.log(`name: ${name}, age: ${age}`);
 }
+
+/**
+ * type 키워드로 타입 별칭 만들기
+ * 
+ * type 새로운 타입 = 기존 타입
+ * 
+ * 매개변수의 개수나 타입, 반환 타입이 다른 함수를 선언하는
+ * 잘못을 미연에 방지할 수 있다.
+ */
+
+type stringNumberFunc = (stringValue: string, numberValue: number) => void;
+let f: stringNumberFunc = function(a: string, b: number): void {};
+let g: stringNumberFunc = function(c: string, d: number): void {};
