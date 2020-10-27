2731cf4d62972e7b12e5194d2aacb8b914db45d2
enginecode-cpu <bassmatics1002@gmail.com>
2020-10-27 21:14:48 +0900

    익명 함수와 const 함수 표현식

--- src/7.함수 표현식.ts
+++ src/7.함수 표현식.ts
@@ -16,3 +16,19 @@ console.log(sum2(1, 10));
 
 let fx = function(a: number, b: number) { return a + b; }
 fx = function(a: number, b: number) { return a - b; }
+
+/**
+ * 익명 함수
+ */
+
+let value = (function(a: number, b: number) { return a + b; })(1, 2);
+console.log(value);
+
+/**
+ * const 키워드와 함수 표현식
+ * 함수 표현식은 let보다는 const 키워드로 선언하는 것이 좋다
+ * 
+ * let은 언젠가 함수가 다른 내용으로 바뀔 수 있다.
+ * 
+ * const는 함수 내용이 절대 바뀔 수 없다.
+ */
