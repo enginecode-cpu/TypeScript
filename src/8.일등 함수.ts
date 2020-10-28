fe0e78f1cae70b88e7baf93233eec0f60fc53893
enginecode-cpu <bassmatics1002@gmail.com>
2020-10-28 18:35:19 +0900

    중첩 함수

--- src/8.일등 함수.ts
+++ src/8.일등 함수.ts
@@ -14,3 +14,17 @@ const init = (callback: () => void): void => {
 }
 
 init(() => console.log('custom initialization finished'))
+
+/**
+ * 중첨 함수
+ * 함수 안에 또 다른 함수를 사용할 수 있다
+ */
+
+const calc = (value: number, callback: (x: number) => void): void => {
+    const add = (a: number, b: number) => a + b
+    function mul(a: number, b: number) { return a * b }
+
+    const res = mul(add(1, 2), value);
+    callback(res)
+}
+calc(30, (res: number) => console.log(`result: ${res}`))
