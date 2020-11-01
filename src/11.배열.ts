e9861577c1906357f3f76990e2201dbc70568892
enginecode-cpu <bassmatics1002@gmail.com>
2020-11-01 17:35:24 +0900

    배열은 객체

---
+++ src/11.배열.ts
@@ -0,0 +1,10 @@
+/**
+ * 배열
+ * 
+ * 자바스크립트에서 배열은 객체이다.
+ */
+
+const arr = [1, 2, 3]
+const object = { name: '짱구', age: 5 }
+
+console.log(`arr: ${Array.isArray(arr)},`, `object: ${Array.isArray(object)}`)
