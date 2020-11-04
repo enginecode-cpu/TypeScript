b1258e577aad96df9fd2d9c577d30c5dac5a8713
enginecode-cpu <bassmatics1002@gmail.com>
2020-11-04 23:42:34 +0900

    filter 메서드

--- src/13.배열의 map, reduce, filter.ts
+++ src/13.배열의 map, reduce, filter.ts
@@ -10,3 +10,26 @@ let tempResult = numbers.filter(val => val % 2 != 0)
                         .reduce(multiply2, 1)
 const result2 = Math.round(Math.sqrt(tempResult))
 console.log(result2)
+
+
+/**
+ * filter 메서드
+ * 배열의 타입이 T[]일 때, 다음과 같이 설계되었다.
+ * 
+ * filter(callback: (value: T, index?: number): boolean): T[]
+ */
+
+/**
+ * 배열을 반으로 분리하기
+ */
+
+import { range } from './range'
+
+const array: number[] = range(1, 10 + 1)
+const center = array.length / 2
+
+const beforeArray: number[] = array.filter((val, index) => index < center)
+const afterArray: number[] = array.filter((val, index) => index >= center)
+
+console.log(beforeArray)
+console.log(afterArray)
