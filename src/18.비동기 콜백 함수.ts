f1b0e8912feb9f364cbae59d3c4d17421abcc662
enginecode-cpu <bassmatics1002@gmail.com>
2020-11-15 16:51:41 +0900

    readFileSync, readFile 추가 설명

--- src/18.비동기 콜백 함수.ts
+++ src/18.비동기 콜백 함수.ts
@@ -10,6 +10,18 @@
  * readFile은(비동기): 프로그램의 동작을 멈추지 않는 대신 결과를 콜백 함수로 얻게 한다.
  */
 
+/**
+ * readFileSync와 readFile API
+ * 
+ * readFileSync는 파일을 읽어서 Buffer라는 타입으로 전달한다.
+ * Buffer는 NodeJS가 제공하는 클래스로서 바이너리 데이터를
+ * 저장하는 기능을 수행한다. Buffer의 데이터를 문자열로 만들 때는
+ * Buffer의 toString 메서드를 사용한다.
+ * 
+ * readFile은 예외가 발생하면 이 예외를 첫 번째 매개변수에 전달한다.
+ */
+
+
 import {readFileSync, readFile} from 'fs';
 
 console.log('read package.json using synchronous api...')
@@ -17,8 +29,11 @@ const buffer: Buffer = readFileSync('./package.json')
 console.log(buffer.toString())
 
 readFile('./package.json', (error: Error, buffer: Buffer) => {
-  console.log('read package.json using asynchronous api...')
-  console.log(buffer.toString())
+  if (error) throw error
+  else {
+    console.log('read package.json using asynchronous api...')
+    console.log(buffer.toString())
+  }
 })
 
 const readFilePromise = (filename: string): Promise<string> => {
